165be416c7d9946b966bbfa414a86324fc08f518
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-17 14:28:50 +0100

    feat(auth-layout): add new authentication layout component with HTML, CSS, and test files
    - Created auth-layout component with responsive design.
    - Added HTML structure for the layout including logo and background image.
    - Implemented CSS for styling the layout.
    - Added unit tests to ensure component creation.

--- frontend/latencyzero_client/src/app/app.routes.ts
+++ frontend/latencyzero_client/src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout-component/main-layout-component';
 import { HardVisionPage } from './features/hardvision/pages/hard-vision-page/hard-vision-page';
+import { HomePage } from './layout/home-page/home-page';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent,
+    component: HomePage,
     children: [
       {
         path: 'hardvisionai',
         component: HardVisionPage
-      }
+      },
+
     ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
   }
 ];
--- frontend/latencyzero_client/src/app/auth/auth.routes.ts
+++ frontend/latencyzero_client/src/app/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from "@angular/router";
 import { MainLayoutComponent } from "../layout/main-layout-component/main-layout-component";
+import { AuthLayout } from "./layout/auth-layout/auth-layout";
 
 export const authRoutes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent,
+    component: AuthLayout,
     children: [
       {
         path: 'login',
