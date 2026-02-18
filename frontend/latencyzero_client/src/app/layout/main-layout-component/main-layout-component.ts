897322f9b839d8114a453009b9b161957e2bd69b
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-18 20:23:40 +0100

    feat: implement footer component and integrate into main layout

--- frontend/latencyzero_client/src/app/layout/main-layout-component/main-layout-component.ts
+++ frontend/latencyzero_client/src/app/layout/main-layout-component/main-layout-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from "@angular/router";
+import { FooterComponent } from "../components/footer-component/footer-component";
 
 @Component({
   selector: 'app-main-layout-component',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './main-layout-component.html',
   styleUrl: './main-layout-component.css',
 })
