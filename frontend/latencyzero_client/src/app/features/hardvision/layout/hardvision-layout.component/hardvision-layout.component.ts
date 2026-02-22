bac5e1c56689ba623bd0774391f49bfe4ff7f1eb
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2026-02-22 12:34:20 +0100

    feat: add footer in hardvision layout

--- frontend/latencyzero_client/src/app/features/hardvision/layout/hardvision-layout.component/hardvision-layout.component.ts
+++ frontend/latencyzero_client/src/app/features/hardvision/layout/hardvision-layout.component/hardvision-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HardVisionPage } from '../../pages/hard-vision-page/hard-vision-page.component';
 import { AuthLayout } from "../../../../auth/layout/auth-layout/auth-layout";
+import { FooterComponent } from "../../../../layout/components/footer-component/footer-component";
 
 @Component({
   selector: 'app-hardvision-layout.component',
-  imports: [HardVisionPage, AuthLayout],
+  imports: [HardVisionPage, AuthLayout, FooterComponent],
   templateUrl: './hardvision-layout.component.html',
   styleUrl: './hardvision-layout.component.css',
 })
