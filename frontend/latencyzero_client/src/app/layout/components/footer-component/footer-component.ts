1fcffb9e6df9d7716c84fddd71c8da4deb50ef04
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-22 19:15:09 +0100

    feat: enhance footer component with social media links and improved layout

--- frontend/latencyzero_client/src/app/layout/components/footer-component/footer-component.ts
+++ frontend/latencyzero_client/src/app/layout/components/footer-component/footer-component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SocialMedia } from "../../../shared/components/social-media/social-media";
 
 @Component({
   selector: 'footer-component',
-  imports: [],
+  imports: [SocialMedia, RouterLink],
   templateUrl: './footer-component.html',
   styleUrl: './footer-component.css',
 })
 export class FooterComponent {
-
+  readonly year = new Date().getFullYear();
 }
