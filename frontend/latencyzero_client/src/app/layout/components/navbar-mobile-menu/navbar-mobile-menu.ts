6b7bac625056673594d3d147093fb57deca12615
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-22 18:41:09 +0100

    feat: implement mobile navbar menu and update navbar component

---
+++ frontend/latencyzero_client/src/app/layout/components/navbar-mobile-menu/navbar-mobile-menu.ts
@@ -0,0 +1,37 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { JwtService } from '../../../core/services/jwt.service';
+
+@Component({
+  selector: 'navbar-mobile-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar-mobile-menu.html',
+  styleUrl: './navbar-mobile-menu.css',
+})
+export class NavbarMobileMenuComponent {
+
+  private authService = inject(AuthService);
+  private jwtService = inject(JwtService);
+  username = this.jwtService.getName() ?? 'Usuario';
+
+  isOpen = signal(false);
+  isLoggedIn = computed(() => this.authService.authStatus() === 'authenticated');
+
+  get userInitial(): string {
+    return this.jwtService.getName()?.charAt(0).toUpperCase() ?? 'U';
+  }
+
+  toggle() {
+    this.isOpen.update(v => !v);
+  }
+
+  close() {
+    this.isOpen.set(false);
+  }
+
+  logout() {
+    this.close();
+    this.authService.logout();
+  }
+}
