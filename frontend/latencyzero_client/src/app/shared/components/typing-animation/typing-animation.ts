8a3fd54b8372d31b50d24b3383aab90df1a9d0ee
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-23 20:02:09 +0100

    feat: implement suggestion chips and typing animation components for enhanced chat interaction

---
+++ frontend/latencyzero_client/src/app/shared/components/typing-animation/typing-animation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'typing-animation',
+  imports: [],
+  templateUrl: './typing-animation.html',
+  styleUrl: './typing-animation.css',
+})
+export class TypingAnimation {
+
+}
