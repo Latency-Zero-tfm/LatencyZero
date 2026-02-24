c6c893344ab2d1f5dc5919ada913e50f05037187
Alvaro Lopez <alvalogue72@gmail.com>
2026-02-24 19:28:55 +0100

    style: add sidebar responsive

--- frontend/latencyzero_client/src/app/admin/layout/admin-layout/admin-layout.ts
+++ frontend/latencyzero_client/src/app/admin/layout/admin-layout/admin-layout.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgClass],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css',
 })
 export class AdminLayout {
+  sidebarOpen = true;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
